152c7dab0c3a26ba60df1f41a917ac9680c152c2
eBdevelpment <ebezerradevelopment@gmail.com>
2025-04-16 11:13:05 -0300

    PreList Refactor

--- src/PreList.php
+++ src/PreList.php
@@ -3,28 +3,16 @@
 
     class PreList {
 
-        public function teste(){
+        protected array $configs;
 
-            $segredo = defined('CONF_TESTE_CONFIG') ? CONF_TESTE_CONFIG : null;
-
-            if (!$segredo) {
-                throw new \Exception("Chave CONF_TESTE_CONFIG não definida");
-            }
-    
-            // Usar $segredo normalmente...
-            return "Usando o segredo: " . $segredo;
-
+        public function __construct($configs = [])
+        {
+            $this->configs = $configs;
         }
 
         public function preLista($data, $tipo){
 
-            $url = defined('CONF_API_CLIENT_KEYCONF_API_PREPOSICOES_URL') ? CONF_API_CLIENT_KEYCONF_API_PREPOSICOES_URL : null;
-            $token = defined('CONF_API_PREPOSICOES_TOKEN') ? CONF_API_PREPOSICOES_TOKEN : null; 
-            $auth  = defined('CONF_API_PREPOSICOES_AUTHORIZATION') ? CONF_API_PREPOSICOES_AUTHORIZATION : null; 
-            $secret  = defined('CONF_API_CLIENT_KEY') ? CONF_API_CLIENT_KEY : null; 
-
-    
-            $tokenEncoded = base64_encode($token);
+            $tokenEncoded = base64_encode($this->configs["token"]);
     
             $postFields = [
                 'token' => $tokenEncoded,
@@ -34,13 +22,13 @@
             ];
     
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_URL, $this->configs["url"]);
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch,  CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                "Authorization: ".$auth." " . base64_encode($secret)
+                "Authorization: ".$this->configs["auth"]." " . base64_encode($this->configs["secret"])
             ]);
     
             $response = curl_exec($ch);
@@ -61,30 +49,24 @@
 
         public function preMinerar($data, $tipo, $tema){
 
-            $url = defined('CONF_API_CLIENT_KEYCONF_API_PREPOSICOES_URL') ? CONF_API_CLIENT_KEYCONF_API_PREPOSICOES_URL : null;
-            $token = defined('CONF_API_PREPOSICOES_TOKEN') ? CONF_API_PREPOSICOES_TOKEN : null; 
-            $auth  = defined('CONF_API_PREPOSICOES_AUTHORIZATION') ? CONF_API_PREPOSICOES_AUTHORIZATION : null; 
-            $secret  = defined('CONF_API_CLIENT_KEY') ? CONF_API_CLIENT_KEY : null; 
-
-    
-            $tokenEncoded = base64_encode($token);
+            $tokenEncoded = base64_encode($this->configs["token"]);
     
             $postFields = [
                 'token' => $tokenEncoded,
                 'sessao' => 'pre-mineracao',
                 'palavras' => $data,
-                'tipo_campanha' => $tipo, //(A = campanha aberta / F = campanha fechada)
+                'tipo_campanha' => $tipo, //(A = aberta / F = fechada)
                 'tema_central' => $tema
             ];
-    
+
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_URL, $this->configs["url"]);
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch,  CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                "Authorization: ".$auth." " . base64_encode($secret)
+                "Authorization: ".$this->configs["auth"]." " . base64_encode($this->configs["secret"])
             ]);
     
             $response = curl_exec($ch);
@@ -102,6 +84,5 @@
             }
 
         }
-
 
     }
